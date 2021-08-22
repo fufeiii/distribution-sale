2b300dc45fcd82c6d0767c6b871f8cc4c94309a8
FuFei <848125851@qq.com>
2021-08-22 22:36:10 +0800

    初始化项目

---
+++ ds-admin/src/main/java/cn/fufeii/ds/admin/DsAdminApplication.java
@@ -0,0 +1,19 @@
+package cn.fufeii.ds.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author FuFei
+ * @date 2021/8/22
+ */
+@SpringBootApplication
+public class DsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsAdminApplication.class);
+    }
+
+}
---
+++ ds-dao/src/main/java/cn/fufeii/ds/repository/package-info.java
@@ -0,0 +1,1 @@
+package cn.fufeii.ds.repository;
