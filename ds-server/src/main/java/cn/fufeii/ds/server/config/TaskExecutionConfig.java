7c628541fc2a4e538406bc1b0cddc9f3a9667fd4
FuFei <848125851@qq.com>
2022-04-09 15:47:31 +0800

    优化：异步任务异常日志打印

--- ds-server/src/main/java/cn/fufeii/ds/server/config/TaskExecutionConfig.java
+++ ds-server/src/main/java/cn/fufeii/ds/server/config/TaskExecutionConfig.java
@@ -1,9 +1,12 @@
 package cn.fufeii.ds.server.config;
 
 import com.alibaba.ttl.TtlRunnable;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.boot.task.TaskExecutorCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -14,14 +17,24 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @date 2022/3/12
  */
 @Configuration
-public class TaskExecutionConfig {
+public class TaskExecutionConfig implements AsyncConfigurer {
 
+    /**
+     * 配置spring任务线程池
+     * 设置拒绝策略
+     * 设置ttl的包装器
+     */
     @Bean
     public TaskExecutorCustomizer taskExecutorCustomizer() {
         return taskExecutor -> {
             taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
             taskExecutor.setTaskDecorator(TtlRunnable::get);
         };
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
     }
 
 }
